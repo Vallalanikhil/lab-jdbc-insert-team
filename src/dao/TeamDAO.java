bba67a29a33c92130cff1e2706ebc66af962b0b2
Nikhil <vallalanikhil@gmail.com>
2020-08-19 18:32:26 +0530

    done

--- src/dao/TeamDAO.java
+++ src/dao/TeamDAO.java
@@ -1,1 +1,30 @@
+package dao;
+
+import model.Team;
+import utility.ConnectionManager;
+
+public class TeamDAO{
+	
+	public void createTeam(Team team) {
+		
+		Integer id= team.getId();
+		String name=team.getName();
+		String Couch=team.getCoach();
+		int captain=team.getCaptain();
+		int city=team.getHome_city();
+		ConnectionManager cm=new ConnectionManager();
+		
+		//insert data into database
+		String sql="insert into TEAM(ID,NAME,COACH,HOME_CITY,CAPTAIN,)VALUES(?,?,?,?,?)";
+		
+		
+		
+		
+	}
+
+	public void createTeam1(Team team) {
+		// TODO Auto-generated method stub
+		
+	}
+}
 
